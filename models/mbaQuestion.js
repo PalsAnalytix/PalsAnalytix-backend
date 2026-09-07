1a71bb98c07674a18561759a52b9cb6b3001b579
PalsAnalytix <palsanalytix.dev@gmail.com>
2026-09-07 15:05:26 +0530

    Update mbaQuestion.js

--- models/mbaQuestion.js
+++ models/mbaQuestion.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 const mbaQuestionSchema = new mongoose.Schema({
+  questionNumber: { type: Number },
   text: { type: String, required: true },
-  options: { type: [String], validate: v => v.length === 4 },
+  options: { type: [String], validate: v => v.length === 3 },
   correctOptionIndex: { type: Number, required: true },
+  questionImage: { type: String },
+  optionImages: {
+    A: { type: String },
+    B: { type: String },
+    C: { type: String },
+  },
+  solution: { type: String },
   tags: [String],
   difficulty: { type: String, enum: ["easy", "medium", "hard"], default: "medium" },
   createdBy: { type: String },
