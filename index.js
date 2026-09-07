10ab522f0725ef79571ade01aefd74381999b717
PalsAnalytix <palsanalytix.dev@gmail.com>
2026-09-07 15:18:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -976,6 +976,7 @@ app.use("/api/mba/auth", require("./routes/mbaAuth"));
 app.use("/api/mba/dev", require("./routes/mbaSeed"));
 app.use("/api/mba/admin", authenticateUser, isAdmin, require("./routes/mbaAdmin"));
 app.use("/api/mba/admin/questions", authenticateUser, isAdmin, require("./routes/mbaQuestions"));
+app.use("/api/mba/admin/upload", authenticateUser, isAdmin, require("./routes/mbaUpload"));
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
